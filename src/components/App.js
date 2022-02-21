1f84651d0c4ba2a96b536ed2f88259b622227449
Benoit Balcon <benoit.b.balcon@renault.com>
2022-02-21 18:52:28 +0100

    callbakc function to get videos

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,28 @@
 import React from "react";
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import youtube from "../apis/youtube";
 
-export const App = () => {
-  return <div>App</div>;
-};
+class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({ videos: response.data.items });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+export default App;
